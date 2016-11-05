cc0d2c22c18e0aad579c12615c3e2b9d824c5f6f
Alvaro Cabrera <pateketrueke@gmail.com>
2016-11-05 02:47:57 -0600

    Fixed state and initial settings

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const _SETTINGS = {};
 const _defaults = {
   distTreshold: 90,
   distMax: 120,
-  bodyElement: document.body,
+  bodyElement: 'body',
+  triggerElement: 'body',
   refreshTimeout: 500,
   refreshFunction: () => location.reload(),
   resistanceFunction: t => Math.min(1, t / 2.5),
@@ -22,23 +23,56 @@ let distResisted = 0;
 
 let _state = 'pending';
 let _setup = false;
+let _enable = false;
 let _timeout;
+
+function _closestElement(node, selector) {
+  let depth = 10;
+
+  do {
+    if (!(node && node.tagName) || !depth) {
+      return null;
+    }
+
+    if (node.tagName && node.tagName === selector) {
+      return node;
+    }
+
+    if (selector.charAt() === '#' && node.id === selector.substr(1)) {
+      return node;
+    }
+
+    if (selector.charAt() === '.' && node.classList.contains(selector.substr(1))) {
+      return node;
+    }
+
+    depth -= 1;
+
+    node = node.parentNode;
+  } while (node.parentNode);
+}
 
 function _setupEvents() {
   window.addEventListener('touchstart', (e) => {
+    _enable = _closestElement(e.target, _SETTINGS.triggerElement);
+
     if (_state === 'pending') {
       _SETTINGS.bodyElement.classList.remove('-release');
       _SETTINGS.bodyElement.classList.remove('-refresh');
     }
 
+    clearTimeout(_timeout);
+
     if (!window.scrollY) {
       pullStartY = e.touches[0].screenY;
     }
-
-    clearTimeout(_timeout);
   });
 
   window.addEventListener('touchmove', (e) => {
+    if (!_enable) {
+      return;
+    }
+
     if (!pullStartY) {
       if (!window.scrollY) {
         pullStartY = e.touches[0].screenY;
@@ -86,10 +120,9 @@ function _setupEvents() {
       _SETTINGS.bodyElement.classList.add('-refresh');
     }
 
-    _SETTINGS.bodyElement.style.transform = 'translate3d(0,0,0)';
     _SETTINGS.bodyElement.classList.remove('-release');
     _SETTINGS.bodyElement.classList.remove('-pull');
-    _SETTINGS.bodyElement.classList.remove('-end');
+    _SETTINGS.bodyElement.style.transform = '';
     _state = 'pending';
 
     pullStartY = pullMoveY = null;
