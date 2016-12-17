e3999c159f878c9787b928b990ce9bda5c91bd7a
Doug DeBold <ddebold56@gmail.com>
2016-12-17 14:25:29 -0600

    Use local QUnit installation

--- tests/tests.js
+++ tests/tests.js
@@ -1,24 +1,6 @@
-/* eslint-global QUnit */
-
 const { test } = QUnit;
 
-QUnit.module('Setup & Teardown', {
-  beforeEach(assert) {
-    if (!document.querySelector('#ptr-test-container')) {
-      const ptrContainer = document.createElement('div');
-      ptrContainer.setAttribute('id', 'ptr-test-container');
-
-      const ptrTrigger = document.createElement('div');
-      ptrTrigger.setAttribute('id', 'ptr-trigger-element');
-
-      ptrContainer.appendChild(ptrTrigger);
-
-      document.body.insertBefore(ptrContainer, document.body.firstChild);
-    }
-    assert.ok(document.querySelector('#ptr-test-container'), 'Container el setup');
-    assert.ok(document.querySelector('#ptr-trigger-element'), 'trigger el setup');
-  },
-});
+QUnit.module('Setup & Teardown');
 
 test('Init', (assert) => {
   assert.equal(typeof PullToRefresh.init, 'function', 'Init is a function');
