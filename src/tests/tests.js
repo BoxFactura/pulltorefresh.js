c95a4b5a1a588cbc6fddcef0e502fd82d2ef0b42
Alvaro Cabrera <pateketrueke@gmail.com>
2017-12-27 00:07:07 -0600

    Fixed tests

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -1,33 +1,58 @@
+/* global QUnit, PullToRefresh */
+
 const { test, module } = QUnit;
 
+const _ = PullToRefresh._;
+const $ = selector => document.querySelector(selector);
+const $$ = selector => document.querySelectorAll(selector);
+
 module('Setup & Teardown');
-test('init()', (assert) => {
+test('init() & destroy()', assert => {
   assert.equal(typeof PullToRefresh.init, 'function', 'Init is a function');
 
-  const result = PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
-  assert.ok(document.querySelector('.ptr--ptr'), 'it adds the ptr element');
-  assert.equal(document.querySelectorAll('#pull-to-refresh-js-style').length, 1, 'it adds the style element');
+  const x = PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
 
-  result.destroy();
+  assert.equal(typeof x.destroy, 'function', 'returns a destroy function');
+
+  // attach DOM & events manually
+  _.setupDOM(x);
+
+  assert.ok($('.ptr--ptr'), 'it adds the ptr element');
+  assert.equal($$('#pull-to-refresh-js-style').length, 1, 'it adds the style element');
+
+  x.destroy();
+
+  _.onReset(x);
+
+  const done = assert.async();
+
+  setTimeout(() => {
+    assert.equal($$('.ptr--ptr').length, 0, 'destroy removes the ptr element');
+    assert.equal($$('#pull-to-refresh-js-style').length, 0, 'destroy removes the style element');
+
+    done();
+  }, x.refreshTimeout + 1);
 });
 
-test('destroy()', (assert) => {
-  const result = PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
-  assert.equal(typeof result.destroy, 'function', 'returns a destroy function');
+test('Ensure Init is idempotent', assert => {
+  const x = PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
+  const y = PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
 
-  result.destroy();
-  assert.equal(document.querySelectorAll('.ptr--ptr').length, 0, 'destroy removes the ptr element');
-  assert.equal(document.querySelectorAll('#pull-to-refresh-js-style').length, 0, 'destroy removes the style element');
-});
+  _.setupDOM(x);
+  _.setupDOM(y);
 
-test('Ensure Init is idempotent', (assert) => {
-  const result = PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
-  assert.equal(document.querySelectorAll('.ptr--ptr').length, 1, 'it adds ptr element');
-  assert.equal(document.querySelectorAll('#pull-to-refresh-js-style').length, 1, 'it adds the style element');
+  assert.equal($$('.ptr--ptr').length, 2, 'add two ptr elements');
+  assert.equal($$('#pull-to-refresh-js-style').length, 1, 'adds the style element once');
 
-  PullToRefresh.init({ mainElement: '#ptr-trigger-element', triggerElement: '#ptr-trigger-element' });
-  assert.equal(document.querySelectorAll('.ptr--ptr').length, 1, 'calling init() a second time does not create another ptr node');
-  assert.equal(document.querySelectorAll('#pull-to-refresh-js-style').length, 1, 'calling init() a second time does not create another style node');
+  const done = assert.async();
 
-  result.destroy();
+  _.onReset(x);
+  _.onReset(y);
+
+  setTimeout(() => {
+    assert.equal($$('.ptr--ptr').length, 0, 'remove added ptr elements');
+    assert.equal($$('#pull-to-refresh-js-style').length, 0, 'remove style element');
+
+    done();
+  }, x.refreshTimeout + 2);
 });
