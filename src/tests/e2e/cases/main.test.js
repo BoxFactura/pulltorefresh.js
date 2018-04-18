00bc4c6955d590b72b12ce89135975c46009a7f4
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-04-18 00:28:18 -0500

    WIP

--- src/tests/e2e/cases/main.test.js
+++ src/tests/e2e/cases/main.test.js
@@ -1,8 +1,11 @@
 /* global fixture, test */
 
 fixture('Some test')
-  .page('http://localhost:3001');
+  .page('http://localhost:3001/demos/trigger.html');
 
-test('just wait', async t => {
-  await t.wait(1000);
+test('try to drag', async t => {
+  await t
+    .click('.trigger')
+    .drag('.trigger', 0, 100, { speed: 0.1 })
+    .drag('.trigger', 0, 200, { speed: 0.1 });
 });
