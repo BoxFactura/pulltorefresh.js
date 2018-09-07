9097a6c4a4bff1d2d214cbe1438a47b553c1a1d7
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-09-06 20:18:15 -0500

    Rename all sources; use docs/

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,64 @@
 'use strict';
 
-import _ptrMarkup from './_markup.pug';
-import _ptrStyles from './_styles.less';
+const _ptrMarkup = `
+<div class="__PREFIX__box">
+  <div class="__PREFIX__content">
+    <div class="__PREFIX__icon"></div>
+    <div class="__PREFIX__text"></div>
+  </div>
+</div>
+`;
+
+const _ptrStyles = `
+.__PREFIX__ptr {
+  box-shadow: inset 0 -3px 5px rgba(0, 0, 0, 0.12);
+  pointer-events: none;
+  font-size: 0.85em;
+  font-weight: bold;
+  top: 0;
+  height: 0;
+  transition: height 0.3s, min-height 0.3s;
+  text-align: center;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-end;
+  align-content: stretch;
+}
+
+.__PREFIX__box {
+  padding: 10px;
+  flex-basis: 100%;
+}
+
+.__PREFIX__pull {
+  transition: none;
+}
+
+.__PREFIX__text {
+  margin-top: .33em;
+  color: rgba(0, 0, 0, 0.3);
+}
+
+.__PREFIX__icon {
+  color: rgba(0, 0, 0, 0.3);
+  transition: transform .3s;
+}
+
+/*
+When at the top of the page, disable vertical overscroll so passive touch
+listeners can take over.
+*/
+.__PREFIX__top {
+  touch-action: pan-x pan-down pinch-zoom;
+}
+
+.__PREFIX__release {
+  .__PREFIX__icon {
+    transform: rotate(180deg);
+  }
+}
+`;
 
 const _defaults = {
   distThreshold: 60,
