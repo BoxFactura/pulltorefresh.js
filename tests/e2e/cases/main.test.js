58ed29006cf027e408015fd88cc744d4762566d0
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-10-02 22:26:23 -0500

    Enable TouchEmulator() if NODE_ENV is 'test'; fix

--- tests/e2e/cases/main.test.js
+++ tests/e2e/cases/main.test.js
@@ -1,25 +1,7 @@
-import { ClientFunction } from 'testcafe';
-
 /* global fixture, test */
 
-// FIXME: add this to demos UI for toggling?
-const getTouchEmulator = ClientFunction(() => {
-  const s = document.createElement('script');
-  s.src = '//unpkg.com/hammer-touchemulator@0.0.2/touch-emulator.js';
-  document.getElementsByTagName('head')[0].appendChild(s);
-  return new Promise(ok => {
-    setTimeout(() => {
-      window.TouchEmulator();
-      ok();
-    }, 1000);
-  });
-});
-
 fixture('Some test')
-  .page('http://localhost:8080/trigger.html')
-  .beforeEach(async t => {
-    await getTouchEmulator();
-  });
+  .page('http://localhost:8080/trigger.html');
 
 test('try to drag', async t => {
   await t
